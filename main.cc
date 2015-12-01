a23372ed714a2189ae2b9159553f6377964a2cc0
gharbi07 <dassi7379@gmail.com>
2015-12-01 15:48:29 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -6,6 +6,9 @@ void main()
 {
   cout << "hello" << endl;
     cout << "Hiii" << endl;
+    
+    
+  cout << "dassi gharbi" << endl;
 
   cout << "end of mini project!" << endl;
   cout << "Master 2 IA" << endl;
