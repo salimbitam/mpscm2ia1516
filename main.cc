f79caaf74aee5555606f74fb6c372e343feec3de
nourelhoudaa <houda.zerarka@gmail.com>
2015-12-01 16:04:05 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,9 +4,6 @@
 using namespace std;
 void main()
 {
-  cout << "hello" << endl;
-  cout << "end of mini project!" << endl;
-  cout <<"goodbye!!"<< endl;
-  cout <<"TP VRP c++"<< endl;
-  cout <<"update"<< end1;
+  cout << "hello in my branch" << endl;
+  
 }
