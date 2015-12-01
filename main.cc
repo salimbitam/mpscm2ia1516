311ca6a91cc80165c1ce38e3eb9a12e2cb3c9131
MiloudiLina <Rosesrose93@gmail.com>
2015-12-01 15:51:08 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -5,11 +5,9 @@ using namespace std;
 void main()
 {
   cout << "hello" << endl;
-    cout << "Hiii" << endl;
+    
 
   cout << "end of mini project!" << endl;
   cout << "Master 2 IA" << endl;
   cout << "Master 2 IA" << endl;
-  cout << "Hi" << endl;
-  cout << "Hello" << endl;
 }
