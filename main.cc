334fe2d3e4240b74cbb732e8b9e9fcfb2e399040
GUETTALaManel <manelguettala@yahoo.fr>
2016-01-26 22:38:04 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,14 +4,4 @@
 using namespace std;
 void main()
 {
-  cout << "   "<< endl;
-  cout << "hello" << endl;
-   cout << "helloooooo!!!" << endl;
-    cout<<"salamo 3alaycom";
-    
-    cout << "zraari" << endl;
-
-  cout << "end of mini project!" << endl;
-  cout << "Master 2 IA" << endl;
-  cout << "Master 2 IA" << endl;
 }
